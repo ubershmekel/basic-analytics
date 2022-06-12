f879b9d400faeef1012d7f80dce67da9a289d761
Yuval Greenfield <ubershmekel@gmail.com>
2022-06-12 02:04:07 -0700

    Send window.basicAnalyticsData events

--- client/src/sdk.ts
+++ client/src/sdk.ts
@@ -14,51 +14,108 @@ export interface EventReturn {
 
 export interface InitData {
   apiBase: string;
-  sessionId: string;
+  sessionId?: string;
 }
 
-let apiData: InitData | null = null;
-
-export function init(data: InitData) {
-  apiData = data;
+interface WindowData {
+  apiBase: string;
+  buffer: EventData[];
 }
 
-const sidKey = "tsauid";
-let sessionId = localStorage.getItem(sidKey);
-if (!sessionId) {
-  sessionId = Math.random().toString(16).slice(2);
-  localStorage.setItem(sidKey, sessionId);
+class BAEndpoint {
+  sidKey = "tsauid";
+  apiData: InitData;
+
+  constructor(apiData: InitData) {
+    this.apiData = apiData;
+  }
+
+  setupSessionId() {
+    if (this.apiData.sessionId) {
+      return;
+    }
+    let sessionId = localStorage.getItem(this.sidKey);
+    if (!sessionId) {
+      sessionId = Math.random().toString(16).slice(2);
+      localStorage.setItem(this.sidKey, sessionId);
+    }
+    this.apiData.sessionId = sessionId;
+  }
+
+  async sendEvent(data: EventData): Promise<EventReturn> {
+    if (!this.apiData?.apiBase) {
+      const msg =
+        "Events are not being sent. Did you create a `BAEndpoint` with valid data?";
+      console.error(msg);
+      return {
+        error: msg,
+      };
+    }
+    if (!data.sessionId) {
+      data.sessionId = this.apiData.sessionId;
+    }
+    if (!data.domain) {
+      data.domain = window.location.hostname;
+    }
+    if (data.key === "pageview" && !data.sv) {
+      data.sv = window.location.href;
+    }
+    const url = this.apiData.apiBase + "/event";
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const result: EventReturn = await response.json();
+    return result;
+  }
 }
 
-export async function sendEvent(data: EventData): Promise<EventReturn> {
-  if (!apiData) {
-    const msg =
-      "Events are not being sent. Did you call tasanlytics `init` with valid data?";
-    console.error(msg);
-    return {
-      error: msg,
-    };
+export default class BasicAnalytics {
+  // A class that helps us have a global analytics instance
+  // for convenience.
+  static endPoint: BAEndpoint | null = null;
+
+  static init(data: InitData) {
+    this.endPoint = new BAEndpoint(data);
+    if (!this.endPoint.apiData.sessionId) {
+      this.endPoint.setupSessionId();
+    }
   }
-  if (!data.sessionId) {
-    data.sessionId = apiData.sessionId;
+
+  static async sendEvent(data: EventData): Promise<EventReturn> {
+    if (!this.endPoint?.apiData?.apiBase) {
+      const msg =
+        "Events are not being sent. Did you call tasanlytics `init` with valid data?";
+      console.error(msg);
+      return {
+        error: msg,
+      };
+    }
+    return this.endPoint.sendEvent(data);
   }
-  if (!data.domain) {
-    data.domain = window.location.hostname;
+
+  static windowInit() {
+    const windowData = (window as any).basicAnalyticsData as WindowData;
+    if (!windowData || !windowData.apiBase) {
+      return;
+    }
+    // rate limiting event sending
+    const msPerSendEvent = 100;
+    this.init({
+      apiBase: windowData.apiBase,
+    });
+    setInterval(() => {
+      if (windowData.buffer.length === 0) {
+        return;
+      }
+      const nextEvent = windowData.buffer.shift()!;
+      this.endPoint?.sendEvent(nextEvent);
+    }, msPerSendEvent);
   }
-  if (data.key === "pageview" && !data.sv) {
-    data.sv = window.location.href;
-  }
-  const url = apiData.apiBase + "/event";
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      Accept: "application/json, text/plain, */*",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-  const result: EventReturn = await response.json();
-  return result;
 }
 
-sendEvent({ key: "pageview" });
+BasicAnalytics.windowInit();
